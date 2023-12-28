af5dbd33b1acdcac4cb645c619efb339a0350956
Phaedra <71579580+ph4iry@users.noreply.github.com>
2023-12-27 19:55:43 -0500

    test commit

--- app/catalog/courses/[code]/page.tsx
+++ app/catalog/courses/[code]/page.tsx
@@ -13,6 +13,7 @@ export default async function CourseView({ params }: {params: { code: string }})
   const course = sheet.find(item => item.code === params.code)!;
 
   const media = categorize(course!.link);
+  console.log(course);
   return (
     <div>
       <a href="/catalog" className="flex text-md items-center font-medium group w-fit"><ArrowLeftIcon className="h-6 mr-1 group-hover:mr-2 transition-all stroke-2" /> Course Catalog</a>
